a6493cc9ac7f516889cca7efdcce88d58f949491
western85 <84554545+western85@users.noreply.github.com>
2022-07-11 22:50:35 -0700

    changed project name to homie-hookup

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const path = require('path')
 
+mongoose.connect('mongodb://localhost:27017/homie-hookup',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('MONGO connection is open!')
+})
+.catch(err => {
+    console.log("Yikes- MONGO connection error!")
+    console.log(err)
+})
+
 app.set('view engine', 'ejs'); 
-app.set('views', path.join(__dirname,'views'));
+app.set('views', path.join(__dirname,'/views'));
 
 app.get('/', (req, res) => {
     res.render('home',)
